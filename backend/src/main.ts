ff472b826a264d62789df31413c5f6b3111f231f
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-10-15 14:06:25 +0900

    📝 Prettier 설정 변경 및 Swagger 의존성 추가 명시

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('API 문서')
-    .setDescription('Nest.js + Swagger 예시 문서입니다')
+    .setDescription('Nest.js + Swagger 예시 문서.')
     .setVersion('1.0')
     .addTag('users') // 선택사항: API 그룹 태그
     .build();
