f49166196b5214658b240a3d103329f4df84572d
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-10-15 14:01:25 +0900

    백엔드 추가

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API 문서')
+    .setDescription('Nest.js + Swagger 예시 문서입니다')
+    .setVersion('1.0')
+    .addTag('users') // 선택사항: API 그룹 태그
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
