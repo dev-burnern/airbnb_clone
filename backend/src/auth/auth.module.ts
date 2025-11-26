7a1a087e09ad0395eed7c198a69e92a61456cd61
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-11-26 17:01:25 +0900

    feat: 인증 시스템 구현 (JWT, OAuth)

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { GithubStrategy } from './strategies/github.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET', 'secretKey'),
+                signOptions: { expiresIn: '60m' },
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    providers: [AuthService, LocalStrategy, JwtStrategy, GithubStrategy],
+    controllers: [AuthController],
+    exports: [AuthService],
+})
+export class AuthModule { }
---
+++ backend/src/auth/strategies/github.strategy.ts
@@ -0,0 +1,33 @@
+import { Strategy } from 'passport-github2';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
+    constructor(
+        configService: ConfigService,
+        private authService: AuthService,
+    ) {
+        super({
+            clientID: configService.get<string>('GITHUB_CLIENT_ID', 'client_id'),
+            clientSecret: configService.get<string>('GITHUB_CLIENT_SECRET', 'client_secret'),
+            callbackURL: 'http://localhost:8080/auth/github/callback',
+            scope: ['user:email'],
+        });
+    }
+
+    async validate(accessToken: string, refreshToken: string, profile: any, done: Function) {
+        const { id, username, emails, photos } = profile;
+        const user = {
+            githubId: id,
+            email: emails[0].value,
+            name: username,
+            avatarUrl: photos[0].value,
+            accessToken,
+        };
+        const payload = await this.authService.validateGithubUser(user);
+        done(null, payload);
+    }
+}
