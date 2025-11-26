8601567dd86b43ea60592e8e456c7f32ba93b91c
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-11-27 06:14:30 +0900

    🐛 Chat 모듈 추가 및 import 경로 수정

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { PaymentsService } from '../payments/payments.service';
-import { PaymentsController } from '../payments/payments.controller';
+import { PaymentsService } from './payments.service';
+import { PaymentsController } from './payments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking } from '../bookings/booking.entity';
 
