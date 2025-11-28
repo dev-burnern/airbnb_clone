13b3d3f2e37bcd44e8081e88389277afa5d5b1e4
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-11-28 15:08:20 +0900

    ✨ Feat(chat): 채팅방 엔티티 및 모듈 구현
    - ChatRoom 엔티티 생성 (호스트, 게스트, 숙소 관계 설정)

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ChatRoom } from './chat.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChatRoom])],
   controllers: [ChatController],
   providers: [ChatService],
 })
-export class ChatModule {}
+export class ChatModule { }
